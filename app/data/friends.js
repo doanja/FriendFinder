fe07d4a75ab17abc7eac52dc7e1c0bbb3cdbb1d9
John Doan <doanja@plu.edu>
2019-11-09 22:10:43 -0800

    added functions to handle posting of new profile

---
+++ app/data/friends.js
@@ -0,0 +1,3 @@
+const profiles = [{ name: 'john', photo: 'asldjkaskdf', scores: [1, 2, 23, 4, , 5, 1, 4, 5] }];
+
+module.exports = profiles;
--- server.js
+++ server.js
@@ -1,2 +1,35 @@
 const express = require('express');
 const path = require('path');
+const profiles = require('./app/data/friends');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Sets up the Express app to handle data parsing (needed to process POST request)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// ===========================================================
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './app/public/home.html'));
+});
+
+app.get('/survey', function(req, res) {
+  res.sendFile(path.join(__dirname, './app/public/survey.html'));
+});
+
+app.get('/api/friends', function(req, res) {
+  return res.json(profiles);
+});
+
+// Create New Profiles - takes in JSON input
+app.post('/api/friends', function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  console.log('req.body:', req.body);
+  profiles.push(req.body);
+  res.json(req.body);
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
